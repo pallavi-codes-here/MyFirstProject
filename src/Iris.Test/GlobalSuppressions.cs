11257eba5202f9fb1e9b6d20161a83eae797c4f4
Pallavi <pallavi.offers@gmail.com>
2023-06-15 23:02:40 -0700

    adding fluent assertions

---
+++ src/Iris.Test/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Pallavi Srivastava. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0058:Expression value is never used", Justification = "FluentAssertions")]
--- src/Iris.Test/UnitTest1.cs
+++ src/Iris.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 // Copyright (c) Pallavi Srivastava. All rights reserved.
 
+using FluentAssertions;
+
 namespace Iris.Test;
 
 public class UnitTest1
@@ -8,6 +10,7 @@ public class UnitTest1
     public void Test1()
     {
         var c = new Class1();
-        Assert.Equal("Iris.Class1", c.ToString());
+
+        c.ToString().Should().Be("Iris.Class1");
     }
 }
